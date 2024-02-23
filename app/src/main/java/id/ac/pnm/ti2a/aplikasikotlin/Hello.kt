5a466b98bab2623d5e85d915972cd77859c1463a
Tria Wilujeng Rahayu Br Sitepu <tria25896@gmail.com>
2024-02-23 10:14:03 +0700

    aplikasi kotlin 28

--- app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/Hello.kt
+++ app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/Hello.kt
@@ -1,2 +1,17 @@
 package id.ac.pnm.ti2a.aplikasikotlin
 
+
+
+fun main(args: Array<String>) {
+    biodata("Tria Wilujeng Rahayu br Sitepu", institusi = "Teknologi Informasi",Hobby="Membaca", Desc = "Saya suka membaca webtoon,wattpad,AU Universe,saya selalu membaca disaat ada waktu luang .")
+}
+
+fun biodata(nama:String, institusi:String, Hobby:String, Desc: String, kelas:String = "2A") {
+    println("Nama  : $nama")
+    println("Prodi : $institusi")
+    println("Kelas : $kelas")
+    println("Hobby : $Hobby")
+    println("$Desc")
+}
+
+
