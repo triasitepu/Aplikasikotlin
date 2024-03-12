3f2ddca6bccc5dd16fc0de95bd0b6a5919f8ddf3
Tria Wilujeng Rahayu Br Sitepu <tria25896@gmail.com>
2024-03-12 09:33:17 +0700

    menambahkan tugas membuat kelas dan objek

--- app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/example/myapp/main.kt
+++ app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin/example/myapp/main.kt
@@ -1,22 +1,21 @@
 package id.ac.pnm.ti2a.aplikasikotlin.example.myapp
-fun buildAquarium() {
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
-    // default height and length
-    val aquarium2 = Aquarium(width = 20)
-    aquarium2.printSize()
-    // default width
-    val aquarium3 = Aquarium(height = 31, length = 100)
-    aquarium3.printSize()
-    // everything custom
-    val aquarium4 = Aquarium(width = 20, height = 31, length = 100)
-    aquarium4.printSize()
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
 
+import java.lang.Math.PI
+
+fun buildAquarium() {
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
-    fun main() {
-        buildAquarium()
-    }
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+
